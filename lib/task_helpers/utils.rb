9f0aa05d7fc005bc20cfd8114dd19b2ebcfa035c
Jesús Torres <jmtorres@ull.es>
2020-12-08 21:03:13 +0000

    Arreglar copia de archivos media

--- lib/task_helpers/utils.rb
+++ lib/task_helpers/utils.rb
@@ -5,14 +5,13 @@ module Utils
     def copy_files(files, destination_directory, base_directory)
         files.each do |pathname|
             destination = pathname.sub(%r(^#{base_directory}), destination_directory)
-            destination_directory = File.dirname(destination)
 
             if File.exist? destination
                 next if [File.ctime(pathname), File.mtime(pathname)].max <= \
                     [File.ctime(destination), File.mtime(destination)].max
             end
             
-            Rake::FileUtilsExt.mkdir_p destination_directory
+            Rake::FileUtilsExt.mkdir_p File.dirname(destination)
             Rake::FileUtilsExt.cp pathname, destination
         end
     end
