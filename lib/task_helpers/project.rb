b23b6875c0ed303e027484244321ccb780f6d226
Jesús Torres <jmtorres@ull.es>
2020-12-08 17:40:17 +0000

    Añadir .mp4 a los contenidos multimedia

--- lib/task_helpers/project.rb
+++ lib/task_helpers/project.rb
@@ -60,6 +60,7 @@ module Project
     def find_media_files(source_directory)
         FileList[
             File.join(source_directory, "**/media/**.jpg"),
+            File.join(source_directory, "**/media/**.mp4"),
             File.join(source_directory, "**/media/**.png"),
             File.join(source_directory, "**/media/**.svg"),
         ]
