56c1203a34f26ea3c311d717cdf059a0bff19917
Jesús Torres <jmtorres@ull.es>
2023-01-11 12:04:52 +0000

    feat: estimación del tiempo de lectura

---
+++ _extensions/reading-time/language/language-en.lua
@@ -0,0 +1,9 @@
+return {
+    less_than_a_minute = "less than a minute",
+    about = "about",
+    one_hour = "an hour",
+    other_hour = "%d hour",
+    one_minute = "%d minute",
+    other_minute = "%d minute",
+    ampersand = "and"
+}
