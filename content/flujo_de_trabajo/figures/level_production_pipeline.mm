9e85df5c2a2e3e1f7254173e9aafc82ddcfc5437
Jesús Torres <jmtorres@ull.es>
2023-01-14 21:21:06 +0000

    contents: migración de 'flujos de trabajo'
    La migración no esta completa. Falta migrar el diagrama de
    Production Pipeline de Plantuml a mermaid

---
+++ content/flujo_de_trabajo/figures/level_production_pipeline.mm
@@ -0,0 +1,10 @@
+graph LR
+    Concepto(Concepto) --> Preproducción(Preproducción)
+    Preproducción --> Producción(Producción)
+    Producción --> Lanzamiento(Lanzamiento / Entrega)
+    Lanzamiento --> Posproducción(Posproducción)
+    style Concepto fill:#90EE90
+    style Preproducción fill:#ADD8E6
+    style Producción fill:#FFA500
+    style Lanzamiento fill:#f08080
+    style Posproducción fill:#FFD700
