083fe451d62073bf82232a74fcb8a06970efc11d
Jesús Torres <jmtorres@ull.es>
2023-02-15 12:45:41 +0000

    fix: problema con actualización de Google Chrome
    Vamos a usar el Chromium que instala Quarto, que puede estar más probado.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,11 +1,32 @@
 #!/bin/sh
 set -e
 
-# Instalar Chrome porque la versión instalada por Quarto no incluye todas las dependencias.
+# Instalar las dependencias de Chromium porque la versión instalda por Quarto no las incluye
 sudo apt update
-sudo apt install -y wget gnupg
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-sudo apt update
-sudo apt install -y google-chrome-stable --no-install-recommends
+sudo apt install -y --no-install-recommends \
+    libasound2 \
+    libatk-bridge2.0-0 \
+    libatk1.0-0 \
+    libatspi2.0-0 \
+    libcairo2 \
+    libcups2 \
+    libdrm2 \
+    libdbus-1-3 \
+    libgbm1 \
+    libglib2.0-0 \
+    libgtk-3-0 \
+    libnspr4 \
+    libnss3 \
+    libpango-1.0-0 \
+    libx11-6 \
+    libx11-xcb1 \
+    libxcb1 \
+    libxcomposite1 \
+    libxdamage1 \
+    libxext6 \
+    libxfixes3 \
+    libxkbcommon0 \
+    libxrandr2 \
+    libxshmfence1 \
+    fonts-liberation
 sudo rm -rf /var/lib/apt/lists/*
